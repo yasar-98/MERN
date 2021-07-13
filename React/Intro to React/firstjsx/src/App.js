7681c0990efd5241bc499c0d854f9dd17dbccc21
yasar-98 <yasar_98@outlook.sa>
2021-07-13 17:24:03 +0300

    first jsx

---
+++ React/Intro to React/firstjsx/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello Dojo!
+        </p>
+
+        Things I need to do:
+        <ul>
+        <li>*Learn React</li>
+        <li>*Run a marathon</li>
+        <li>*Feed the Doogs</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;

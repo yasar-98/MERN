e414d62189f2ef45cb8f53989b481be0d632d0ae
yasar-98 <yasar_98@outlook.sa>
2021-07-14 16:32:32 +0300

    person card

--- React/class component/propitup/src/components/PersonCard.js
+++ React/class component/propitup/src/components/PersonCard.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 
-class PersonCard extends Component
-
-
-{
+class PersonCard extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age,
+        };
+    }
 
     render() {
-        const { firstName, lastName, age, hairColor } = this.props;
+
+        const { firstName, lastName, age , hairColor } = this.props;
         return (
             <div>
-                <h1>{ firstName}, { lastName }</h1>
-                <p>Age: {age} </p>
+                <h1>{ firstName }, { lastName }</h1>
+                <p>Age: {this.state.age} </p>
                 <p>hair color: {hairColor} </p>
+                <button onClick={ ()=> {this.setState({age:this.state.age+1})}}>Birthday button for { firstName} { lastName }</button>
             </div>
+
         );
             
     }

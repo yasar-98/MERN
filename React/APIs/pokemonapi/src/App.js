cdb69a8bec3d9f78be7bbcb634ba893adea04cab
yasar-98 <yasar_98@outlook.sa>
2021-07-25 14:53:08 +0300

    axios Pokemon API

--- React/APIs/pokemonapi/src/App.js
+++ React/APIs/pokemonapi/src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
     const [pokemons, setPokemons] = useState([]);
 
     const fetchPokemon = () => {
-      fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
-            .then(response => response.json())
-            .then(response => setPokemons(response.results))
-    }
+      // fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
+      //       .then(response => response.json())
+      //       .then(response => setPokemons(response.results))
+      axios.get('https://pokeapi.co/api/v2/pokemon/?limit=807')
+            
+            .then(response=>setPokemons(response.data.results)) 
+      }
     return (
       <>
         <button onClick={fetchPokemon}>Fetch Pokemon </button>

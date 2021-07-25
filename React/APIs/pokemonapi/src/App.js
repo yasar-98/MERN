76c966a13bd91760b9fd98060b8d248583300c75
yasar-98 <yasar_98@outlook.sa>
2021-07-25 11:55:08 +0300

    Pokemon API

---
+++ React/APIs/pokemonapi/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+
+    const [pokemons, setPokemons] = useState([]);
+
+    const fetchPokemon = () => {
+      fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
+            .then(response => response.json())
+            .then(response => setPokemons(response.results))
+    }
+    return (
+      <>
+        <button onClick={fetchPokemon}>Fetch Pokemon </button>
+        <ul>{pokemons.length > 0 && pokemons.map((pokemon, index)=>{
+                return (<li key={index}>{pokemon.name}</li>)
+            })}
+        </ul>
+      </>
+    );
+}
+
+export default App;
